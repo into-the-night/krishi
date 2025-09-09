e4feb3a27027409a2cf1a760b39450cf3662ab9f
anvay <anvatrive2324@gmail.com>
2025-09-09 21:40:17 +0530

    chatbot prompt altered

--- agent/bot.py
+++ agent/bot.py
@@ -10,14 +10,17 @@ class Bot:
 
     def analyse_output(self, diagnosis_result, image: Image, language: str = "en"):
         """
-        Analyse the output from a plant disease and pest detection model and provide a detailed analysis of the plant disease and/or pest to a farmer in a friendly and easy to understand manner.
+        Analyse the output from a plant disease and pest detection model and provide a detailed analysis of the plant disease and/or pest and ways to cure the disease and pest to a farmer in a friendly and easy to understand manner.
         """
         disease_predictions = diagnosis_result[0]['model_1_predictions']['predictions']
         pest_predictions = diagnosis_result[0]['predictions']['predictions']
 
-        prompt = f"""You are a farming expert that has knowledge about varios plant diseases.
+        prompt = f"""You are a farming expert that has knowledge about various plant diseases.
         You are given an output from a plant disease and pest detection model, along with the input image.
-        You need to examine the result and provide a detailed analysis of the plant disease and/or pest to a farmer in a friendly and easy to understand manner.
+        You need to do the following-
+        1.examine the result and provide a detailed analysis of the plant disease and/or pest to a farmer,
+        2.Provide a few economical ways to cure the disease and/or pest.
+        respond in a concise and easy to understand manner.
         The analsysi MUST be in the following language {language}. 
         The model result is as follows:
         Disease Model Predictions: {[f"{disease_prediction.get('class')} with confidence {disease_prediction.get('confidence')}" for disease_prediction in disease_predictions]}
@@ -43,7 +46,7 @@ class Bot:
         
         prompt = f"""You are a farming expert that has knowledge about crop planting and agricultural practices.
         You are having a conversation with a farmer.
-        You are to provide a response to the farmer in a friendly and easy to understand manner.
+        You are to provide a response to the farmer in a  Concise, friendly and easy to understand manner.
         The response MUST be in the following language: {language}.
         
         Here is the conversation history:
